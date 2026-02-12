c919f29346e283c4e9ba679bf2350ab34b0d836c
Shouvik-1999 <krshouvik@gmail.com>
2026-02-12 18:52:26 +0530

    first commit

---
+++ src/test/java/com/tutorialsninja/qa/testcases/Login.java
@@ -0,0 +1,81 @@
+package com.tutorialsninja.qa.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        String browserName = "Chrome";
+        if (browserName.equalsIgnoreCase("chorme")) {
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+
+        } else
+            driver = new FirefoxDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+    @Test(priority = 1)
+    public void verifyLoginwithValidCredentials() {
+        // TODO: implement
+
+        driver.get("https://tutorialsninja.com/demo/");
+        driver.findElement(By.linkText("My Account")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.xpath("//input[@name='email']")).clear();
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("krshouvik@gmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).clear();
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Skar@1999");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+        driver.quit();
+
+    }
+
+    @Test(priority = 2)
+    public void verifyLoginWithInvalidCredentials() {
+
+
+        driver.get("https://tutorialsninja.com/demo/");
+        driver.findElement(By.linkText("My Account")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.xpath("//input[@name='email']")).clear();
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("krshouvik" + GenerateTimeStamp() + "@gmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).clear();
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Skar@1999");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).isDisplayed());
+        driver.quit();
+    }
+
+    public String GenerateTimeStamp() {
+        Date dt = new Date();
+        String timeStamp = dt.toString().replace(" ", "_").replace(":", "_");
+        return timeStamp;
+    }
+}
